31c7f7534be6667df25ef1042a1651277c1f397c
SamuelFDEZS <thggsamuel@gmail.com>
2025-07-01 20:28:33 +0200

    create notebook elements, styles and animations

--- 01-todo-list/src/App.jsx
+++ 01-todo-list/src/App.jsx
@@ -1,9 +1,61 @@
 import { useEffect, useState } from 'react';
 import { NoteLine } from './Components/NoteLine';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
 function App () {
     const [isLightMode, setIsLightMode] = useState(true);
-    const [page, setPage] = useState({ 1: Array(16).fill(null) });
+    const [page, setPage] = useState({
+        1: Array(13).fill(null).map(() => (
+            { marked: false, text: '' }
+        ))
+    });
+
     const [currentPage, setCurrentPage] = useState(1);
+    const [isNotebookOpen, setIsNotebookOpen] = useState(false);
+    const [pageFlips, setPageFlips] = useState(Array(11).fill(false));
+
+    const flipPage = () => {
+        setPageFlips(prev => {
+            const newFlips = [...prev];
+            newFlips[currentPage] = true;
+            return newFlips;
+        });
+    };
+
+    const unFlipPage = () => {
+        setPageFlips(prev => {
+            const newFlips = [...prev];
+            newFlips[currentPage] = false;
+            return newFlips;
+        });
+    };
+    const handleNextPage = () => {
+        if (currentPage > 10) return;
+        if (!page[currentPage + 1]) {
+            const pageCopy = { ...page };
+            pageCopy[currentPage + 1] = Array(13).fill(null).map(() => (
+                { marked: false, text: '' }
+            ));
+            setPage({ ...pageCopy });
+        }
+        flipPage();
+        setCurrentPage(currentPage + 1);
+    };
+
+    const handlePreviousPage = () => {
+        if (currentPage >= 1) setCurrentPage(currentPage - 1);
+        unFlipPage();
+    };
+
+    const handleTextChange = (index, text) => {
+        const pageCopy = [...page[currentPage]];
+        pageCopy[index] = { ...pageCopy[index], text };
+        setPage(prev => ({
+            ...prev,
+            [currentPage]: pageCopy
+        }));
+    };
     const handleColorChange = () => {
         setIsLightMode(!isLightMode);
         console.log(isLightMode);
@@ -20,7 +72,7 @@ function App () {
         <>
             <header className='header'>
                 <aside className='header__logo-container'>
-                    <img src='../src/assets/notebook.png' alt='Icono de cuaderno' className='header__logo-container__icon' />
+                    <a href='#'><img src='../src/assets/notebook.png' alt='Icono de cuaderno' className='header__logo-container__icon' /></a>
                 </aside>
                 <h1 className='header__title'>ToDo List</h1>
                 <div className='header__color-button'>
@@ -33,14 +85,22 @@ function App () {
                 </div>
             </header>
 
-            <div className='notebook-cover' />
-            <main className='notebook'>
-                {
-                    page[currentPage].map((text, i) => {
-                        return <NoteLine text={text} key={i} />;
-                    })
-                }
+            <main className={`notebook ${isNotebookOpen ? 'show-page' : ''}`}>
+                <div className={`notebook__cover ${!isNotebookOpen ? 'cover-closed' : 'cover-opened'}`} />
+                <div className={`notebook__page ${pageFlips[currentPage] ? 'previous-page' : ''}`}>
+                    {
+                        page[currentPage].map((task, i) => {
+                            return <NoteLine text={task.text} key={i} handleTextChange={handleTextChange} index={i} />;
+                        })
+                    }
+                </div>
             </main>
+
+            <div className='notebook-buttons'>
+                <button onClick={handleNextPage} className={`notebook-buttons__button notebook-buttons__button--next ${isNotebookOpen ? 'show-arrow' : ''}`}><FontAwesomeIcon icon={faArrowRight} /></button>
+                <button onClick={handlePreviousPage} className={`notebook-buttons__button notebook-buttons__button--previous ${isNotebookOpen && currentPage > 1 ? 'show-arrow' : ''}`}><FontAwesomeIcon icon={faArrowLeft} /></button>
+                <button className='notebook-buttons__button notebook-buttons__button--close' onClick={() => setIsNotebookOpen(!isNotebookOpen)}>{isNotebookOpen ? 'CLOSE' : 'OPEN'}</button>
+            </div>
         </>
     );
 }
--- 01-todo-list/src/Components/NoteLine.jsx
+++ 01-todo-list/src/Components/NoteLine.jsx
@@ -1,10 +1,30 @@
-export const NoteLine = ({ text, setPage }) => {
+import { useEffect, useState } from 'react';
+
+export const NoteLine = ({ text, handleTextChange, index }) => {
+    const [checked, setChecked] = useState(false);
     const autoGrow = (event) => {
         event.target.style.height = 'auto';
         event.target.style.height = event.target.scrollHeight + 'px';
     };
 
+    // Animation for setting check to false to not show the line through
+    useEffect(() => {
+        if (!text) {
+            setChecked(false);
+        }
+    }, [text]);
+
     return (
-        <textarea onInput={autoGrow} rows='1' className='notebook__note-line' />
+        <div className='notebook__task'>
+            {text && <input type='checkbox' name='note' className='notebook__task__checkbox' checked={checked} onChange={() => setChecked(!checked)} />}
+            <div className={`notebook__task__container ${checked ? 'completed' : ''}`}>
+                <textarea
+                    rows='1' name='note' className='notebook__task__container__note-line' value={text} onInput={(e) => {
+                        autoGrow(e);
+                        handleTextChange(index, e.target.value);
+                    }}
+                />
+            </div>
+        </div>
     );
 };
